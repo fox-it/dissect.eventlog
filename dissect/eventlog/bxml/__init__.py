8e1c3a1d4f603639cf69688d6c72126d329774bc
Miauwkeru <Miauwkeru@users.noreply.github.com>
2026-04-14 11:39:14 +0000

    Move bxml to its own module

---
+++ dissect/eventlog/bxml/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from dissect.eventlog.bxml.bxml import (
+    Bxml,
+    BxmlSub,
+    BxmlToken,
+    BxmlType,
+    EvtxNameReader,
+    Template,
+    WevtNameReader,
+    parse_bxml,
+)
+
+__all__ = (
+    "Bxml",
+    "BxmlSub",
+    "BxmlToken",
+    "BxmlType",
+    "EvtxNameReader",
+    "Template",
+    "WevtNameReader",
+    "parse_bxml",
+)
