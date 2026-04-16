0347611df0d3a946b36b4a3d40a15ea1ee7a2336
Miauwkeru <Miauwkeru@users.noreply.github.com>
2026-04-16 08:41:33 +0000

    Move pytest-benchmark skips to the top of conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,18 +8,6 @@ import pytest
 HAS_BENCHMARK = importlib.util.find_spec("pytest_benchmark") is not None
 
 
-def absolute_path(filename: str):
-    return Path(__file__).parent / filename
-
-
-@pytest.fixture
-def get_absolute_path():
-    def _absolute_path(filename):
-        return absolute_path(filename)
-
-    return _absolute_path
-
-
 def pytest_configure(config: pytest.Config) -> None:
     if not HAS_BENCHMARK:
         # If we don't have pytest-benchmark (or pytest-codspeed) installed, register the benchmark marker ourselves
@@ -33,3 +21,15 @@ def pytest_configure(config: pytest.Config) -> None:
 def pytest_runtest_setup(item: pytest.Item) -> None:
     if not HAS_BENCHMARK and item.get_closest_marker("benchmark") is not None:
         pytest.skip("pytest-benchmark is not installed")
+
+
+def absolute_path(filename: str):
+    return Path(__file__).parent / filename
+
+
+@pytest.fixture
+def get_absolute_path():
+    def _absolute_path(filename):
+        return absolute_path(filename)
+
+    return _absolute_path
